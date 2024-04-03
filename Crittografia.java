cad94da808e1dafbdfabc8ec857a9807af484519
Burdusa-Daniel <136704098+Burdusa-Daniel@users.noreply.github.com>
2024-04-03 12:03:11 +0200

    fine

--- Crittografia.java
+++ Crittografia.java
@@ -5,20 +5,40 @@ public class Crittografia {
         String DIZIONARIO = "abcdefghijklmnopqrstuvwxyz";
         String messaggio = "ciao";
         int shift = 2;
-        ArrayList <Character> risultato=  new ArrayList<>();
+        ArrayList<Character> risultato = new ArrayList<>();
+        ArrayList<Character> risultato1 = new ArrayList<>();
 
-            for(char carattere : messaggio.toCharArray()){
-                if (DIZIONARIO.indexOf(carattere) != 1) {
-                    int posizioneOriginale = DIZIONARIO.indexOf(carattere);
-                    int nuovaPosizione = (DIZIONARIO.length() + posizioneOriginale + shift ) % DIZIONARIO.length(); 
-                    risultato.add(DIZIONARIO.charAt(nuovaPosizione));
-                }else{
-                    risultato.add(carattere);
-                }
+        //modo per criptare il messaggio
+        for (char carattere : messaggio.toCharArray()) {
+            if (DIZIONARIO.indexOf(carattere) != -1) {
+                int posizioneOriginale = DIZIONARIO.indexOf(carattere);
+                int nuovaPosizione = (DIZIONARIO.length() + posizioneOriginale + shift) % DIZIONARIO.length();
+                risultato.add(DIZIONARIO.charAt(nuovaPosizione));
+            } else {
+                risultato.add(carattere);
             }
-            for (char carattere1 : risultato) {
-                System.out.print(carattere1);
+        }
+        //Stampa del messaggio criptato
+        for (char carattere1 : risultato) {
+            System.out.print(carattere1 + ", ");
+        }
+
+        System.out.println();
+
+        //modo per decriptare il messaggio
+        for (char character : risultato) {
+            if (DIZIONARIO.indexOf(character) != -1) {
+                int posizioneOriginale1 = DIZIONARIO.indexOf(character);
+                int nuovaPosizione = (DIZIONARIO.length() + posizioneOriginale1 - shift) % DIZIONARIO.length();
+                risultato1.add(DIZIONARIO.charAt(nuovaPosizione));
+            }else{
+                risultato1.add(character);
             }
-        
+        }
+
+        //stampa del messaggio decriptato
+        for (char character2 : risultato1) {
+            System.out.print(character2 + ", ");
+        }
     }
 }
